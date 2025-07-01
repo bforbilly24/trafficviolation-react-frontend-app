d9d39da1ca77f02f154b1e2032d8735771d9b80c
Halim <halimputra0701@gmail.com>
2025-07-01 17:29:26 +0700

    refactor(core): update dependencies and config as need

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss  from '@tailwindcss/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
     react(), // Use only @vitejs/plugin-react-swc
     tailwindcss(), // Tailwind CSS v4 plugin
+    TanStackRouterVite()
   ],
   resolve: {
     alias: {
